b55ec6fec87c0ca81bd319b7a9650bc9e4e12e11
adaravaks <tseveleva2006@gmail.com>
2023-10-08 14:20:44 +0700

    register view and email messages

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+from django.forms import ModelForm, TextInput, Textarea, FileInput, PasswordInput, CharField, EmailInput, EmailField
+
+
+class RegisterUserForm(UserCreationForm):
+    username = CharField(label='Имя пользователя', widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
+    email = EmailField(label='Адрес электронной почты', widget=EmailInput(attrs={'class': 'form-control', 'placeholder': 'Адрес электронной почты'}))
+    password1 = CharField(label='Пароль', widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+    password2 = CharField(label='Пароль ещё раз', widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль ещё раз'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Имя пользователя'}),
+
+            'email': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Адрес электронной почты'}),
+
+            'password1': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Пароль'}),
+
+            'password2': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Пароль ещё раз'})
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,49 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.views.generic import CreateView
+
+from accounts.forms import RegisterUserForm
+from accounts.tokens import account_activation_token
 
 
-def register(request):
-    return HttpResponse('Registration page')
+def activateEmail(request, user, to_email):
+    mail_subject = 'Подтверждение электронной почты'
+    message = render_to_string('accounts/email-verification-message.html', {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Ах ты ёбаный {user}, быстро пиздуй на свой {to_email} и подтверждай свою поганую регистрацию.', extra_tags='alert-success alert')
+    else:
+        messages.error(request, f'Хуй тебе, а не письмо на {to_email}', extra_tags='alert-danger alert')
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        activateEmail(self.request, user, form.cleaned_data.get('email'))
+        return redirect('login')  # TODO: continue from there
 
 
 class LoginUser(LoginView):
-    pass
+    template_name = 'accounts/login.html'
 
 
 def logout(request):
