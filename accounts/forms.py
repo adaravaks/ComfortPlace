76e2f8b27f5ecbbb712aafc934a574a465c576d6
adaravaks <tseveleva2006@gmail.com>
2023-11-04 20:47:55 +0700

    css-ed login and registration forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,33 +5,12 @@ from django.forms import ModelForm, TextInput, Textarea, FileInput, PasswordInpu
 
 
 class RegisterUserForm(UserCreationForm):
-    username = CharField(label='Имя пользователя', widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
-    email = EmailField(label='Адрес электронной почты', widget=EmailInput(attrs={'class': 'form-control', 'placeholder': 'Адрес электронной почты'}))
-    password1 = CharField(label='Пароль', widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
-    password2 = CharField(label='Пароль ещё раз', widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль ещё раз'}))
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-        widgets = {
-            'username': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Имя пользователя'}),
-
-            'email': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Адрес электронной почты'}),
-
-            'password1': PasswordInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Пароль'}),
-
-            'password2': PasswordInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Пароль ещё раз'})
-        }
+    username = CharField(label='Имя пользователя', widget=TextInput(attrs={'class': 'form-input', 'placeholder': 'Имя пользователя'}))
+    email = EmailField(label='Адрес электронной почты', widget=EmailInput(attrs={'class': 'form-input', 'placeholder': 'Адрес электронной почты'}))
+    password1 = CharField(label='Пароль', widget=PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Пароль'}))
+    password2 = CharField(label='Пароль ещё раз', widget=PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Пароль ещё раз'}))
 
 
 class LoginUserForm(AuthenticationForm):
-    username = CharField(label='Имя пользователя', widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
-    password = CharField(label='Пароль', widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+    username = CharField(label='Имя пользователя', widget=TextInput(attrs={'class': 'form-input', 'placeholder': 'Имя пользователя'}))
+    password = CharField(label='Пароль', widget=PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Пароль'}))
